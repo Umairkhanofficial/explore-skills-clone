9480633e76cd0ad643927cc8646328a4cd6d3559
umairkhanofficial <umairkhansadiq@gmail.com>
2023-11-24 20:56:09 +0500

    xplore skill clone

---
+++ app/(component)/navigation/navigation.tsx
@@ -0,0 +1,42 @@
+ // components/NavigationMenu.js
+ "use client"
+ import { useState } from 'react';
+import Link from 'next/link';
+
+ const NavigationMenu = () => {
+   const [isMenuOpen, setMenuOpen] = useState(false);
+ 
+   const toggleMenu = () => {
+     setMenuOpen(!isMenuOpen);
+   };
+ 
+   return (
+     <div>
+       <button onClick={toggleMenu}>Menu</button>
+ 
+       {isMenuOpen && (
+          <nav>
+          <Link href="/" className="logo">Xplore
+              <i className="fab fa-staylinked"></i>kill
+          </Link>
+          <div className="nav-links" id="navLinks">
+              {/* <!-- reposnive bar open and close --> */}
+              {/* <i className="fa fa-times" onclick="hideMenu()"></i> */}
+              <ul>
+                  <li><Link href="/">Home</Link></li>
+                  <li><Link href="/course">Course</Link></li>
+                  <li><Link href="/blog">Blog</Link></li>
+                  <li><Link href="/about">About</Link></li>
+                  <li><Link href="/contact">Contact</Link></li>
+              </ul>
+          </div>
+           <i className="fa fa-bars" onClick={toggleMenu}></i>
+           <button id='navLinks' onClick={toggleMenu}> hit</button> 
+          {/* <!-- reposnive bar open and close --> */}
+      </nav>
+       )}
+     </div>
+   );
+ };
+ 
+ export default NavigationMenu;
---
+++ app/about/page.tsx
@@ -0,0 +1,66 @@
+import Link from "next/link"
+function About() {
+
+    return (
+            <main>
+        <section className="Sub-header">
+        <nav>
+            <Link href="/" className="logo">Xplore
+                <i className="fab fa-staylinked"></i>kill
+            </Link>
+            <div className="nav-links" id="navLinks">
+                
+                <i className="fa fa-times" ></i>
+                <ul>
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/course.html">Course</Link></li>
+                    <li><Link href="/blog.html">Blog</Link></li>
+                   <li> <Link href="/about">About</Link></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
+            </div>
+            <i className="fa fa-bars"></i>
+        </nav>
+        <h1>About Us</h1>
+    </section>
+
+    {/* About Us Section Start*/}
+
+    <section className="about-us">
+        <div className="row">
+            <div className="about-col">
+                <h1>We are the world's largest University</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque in accusantium molestias maiores fuga vitae eos, ducimus officiis repudiandae voluptatem error. Laboriosam, numquam blanditiis aspernatur, nobis tempora consequatur id aliquam asperiores voluptatum iste animi voluptatem fugiat totam excepturi dolor.</p>
+                <a href="#" className="hero_btn btn">EXPLORE NOW</a>
+            </div>
+            <div className="about-col">
+                <img src="img/about.png" alt=""/>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <section className="footer">
+        <hr/>
+        <h4>About Us</h4>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse minima incidunt odio nam facilis, eligendi
+            <br/> Itaque fugiat cupiditate consectetur. Aliquid ab deserunt exercitationem, illum molestiae
+            dolorem.
+        </p>
+
+        <div className="icons">
+            <i className="fab fa-facebook-f"></i>
+            <i className="fab fa-instagram"></i>
+            <i className="fab fa-twitter"></i>
+            <i className="fab fa-linkedin"></i>
+        </div>
+        <p>Made with <i className="fas fa-heart"></i> by <Link href="#">UK Developer</Link></p>
+        <p>Copyright © 2021 <Link href="index.html">Xplore Skill</Link>. All Rights Reserved</p>
+    </section>
+
+    </main>
+    )
+}
+export default About
